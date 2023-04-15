dd57dda2ea9da24b502ec110ccc2d6b3410cdd42
HEYxJENN <95577576+HEYxJENN@users.noreply.github.com>
2023-04-15 15:39:24 +0400

    directories no content

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import AllMovies from "../Movies/Movies";
+// import  from "../"
+
+// import  from "../"
+
+function App() {
+  console.log("Oh, hi Mark!");
+
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <header className="header">
+      <h3 className="header__sign">ЭТО ХЭДЭР</h3>
+    </header>
+  );
+}
+
+export default Header;
