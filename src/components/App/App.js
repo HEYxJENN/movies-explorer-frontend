8a781971300bf023ade7396675b20f7c973f3733
HEYxJENN <95577576+HEYxJENN@users.noreply.github.com>
2023-04-27 15:42:55 +0400

    fix: css, BEM, layout, resolution

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import { Route } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 import { useHistory } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import ErrorPage from "../ErrorPage/ErrorPage.js";
 import Main from "../Main/Main";
@@ -12,6 +13,8 @@ import Register from "../Auth/Register/Register";
 import Profile from "../Auth/Profile/Profile";
 import AllMovies from "../Movies/Movies";
 import SavedMovies from "../SavedMovies/SavedMovies.js";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 import { useState } from "react";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import BurgerMenu from "../Navigation/Navigation";
@@ -20,6 +23,7 @@ function App() {
   console.log("Oh, hi Mark!");
 
   const history = useHistory();
+  const location = useLocation();
 
   const [loggedIn, setLoggedIn] = useState(false);
   const [isBurgerOpened, setBurgerOpened] = useState(false);
@@ -40,54 +44,66 @@ function App() {
     history.push("/signin");
   };
 
+  // не до конца понимаю как вынести header в зависимости от роута
+  // часть классов, которые я заметил не по БЭМ я переименовл, сайт при этом показывает,
+  // что во всех компонентах- все ок, не понимаю почему
+  // function isHeaderVisible() {
+  //   if (
+  //     location.pathname === "/movies" ||
+  //     location.pathname === "/saved-movies"
+  //   ) {
+  //     return true;
+  //   } else return false;
+  // }
+
   return (
     <div className="body">
-      <div className="page">
-        <main className="content">
-          <CurrentUserContext.Provider>
-            <Switch>
-              <Route path="/movies">
-                <AllMovies onBurger={handleBurgerMenuClick} />
-              </Route>
+      {/* <Header isHeaderVisible={isHeaderVisible} /> */}
+      <main className="page">
+        <CurrentUserContext.Provider>
+          <Switch>
+            <Route path="/movies">
+              <AllMovies onBurger={handleBurgerMenuClick} />
+            </Route>
 
-              <Route path="/profile">
-                <Profile
-                  onBurger={handleBurgerMenuClick}
-                  onEdit={handleEditClick}
-                  onExit={handleExitClick}
-                  isLocked={isInputLocked}
-                />
-              </Route>
+            <Route path="/profile">
+              <Profile
+                onBurger={handleBurgerMenuClick}
+                onEdit={handleEditClick}
+                onExit={handleExitClick}
+                isLocked={isInputLocked}
+              />
+            </Route>
 
-              <Route path="/signin">
-                <Login />
-              </Route>
+            <Route path="/signin">
+              <Login />
+            </Route>
 
-              <Route path="/signup">
-                <Register />
-              </Route>
+            <Route path="/signup">
+              <Register />
+            </Route>
 
-              <Route path="/saved-movies">
-                {/* {loggedIn ? <Redirect to="/" /> : <Redirect to="/reg" />} */}
-                <SavedMovies onBurger={handleBurgerMenuClick} />
-              </Route>
+            <Route path="/saved-movies">
+              {/* {loggedIn ? <Redirect to="/" /> : <Redirect to="/reg" />} */}
+              <SavedMovies onBurger={handleBurgerMenuClick} />
+            </Route>
 
-              <Route path="/404">
-                <ErrorPage />
-              </Route>
+            <Route path="/404">
+              <ErrorPage />
+            </Route>
 
-              <Route path="/">
-                <Main />
-              </Route>
-            </Switch>
+            <Route path="/">
+              <Main />
+            </Route>
+          </Switch>
 
-            <BurgerMenu
-              isOpened={isBurgerOpened}
-              onClose={handleBurgerMenuClick}
-            />
-          </CurrentUserContext.Provider>
-        </main>
-      </div>
+          <BurgerMenu
+            isOpened={isBurgerOpened}
+            onClose={handleBurgerMenuClick}
+          />
+        </CurrentUserContext.Provider>
+      </main>
+      {/* <Footer isVisible={isFooterVisible}/> */}
     </div>
   );
 }
